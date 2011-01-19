900d00c88dc35f82f932f8d8f26a3c9fc52a9ba1
Mitch Bradley <wmb@firmworks.com>
2011-01-19 23:26:40 +0000

    Added "x86" to CPU type recognizer script (alternative i*86)
    git-svn-id: svn://coreboot.org/openfirmware@2125 1552c027-8020-0410-b4b5-a757f869b4ce

--- forth/lib/hostcpu.sh
+++ forth/lib/hostcpu.sh
@@ -4,6 +4,7 @@ case ${MACHNAME} in
     sun4*) CPUNAME=sparc ;;
     ppc)   CPUNAME=powerpc ;;
     i*86)  CPUNAME=x86 ;;
+    x86*)  CPUNAME=x86 ;;
     mips)  CPUNAME=mips ;;
     arm*)  CPUNAME=arm ;;
     sun3)  CPUNAME=m68k ;;
