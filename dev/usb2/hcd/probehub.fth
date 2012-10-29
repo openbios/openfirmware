openbios/openfirmware

commit 5d7b0f7dae723bab446361064577d245cdc5e0f9

OLPC XO-1.75 - rework svn 2398 following review comments, change to a string property for the USB hub port test sequence, tested on A3 with Q4B06jb.
git-svn-id: svn://coreboot.org/openfirmware@2412 1552c027-8020-0410-b4b5-a757f869b4ce

dev/usb2/hcd/probehub.fth
@@ -219,23 +219,18 @@ external
 : hub-selftest  ( hub-dev -- error? )
    to hub-dev                                           ( )
 
-   " hub-port-seq" get-inherited-property  if           ( )
+   " usb-hub-test-list" get-inherited-property  if      ( )
       hub-#ports 1+  1  ?do                             ( )
          i (hub-selftest)  if  true unloop exit  then
       loop                                              ( )
    else                                                 ( propval$ )
-      decode-int nip nip                                ( seq )
-
-      4 0 do
-         dup h# ff000000 and d# 24 rshift h# ff and     ( seq port )
-         dup if                                         ( seq port )
-            (hub-selftest)  if  drop true exit  then
-         else                                           ( seq port )
-            drop                                        ( seq )
-         then                                           ( seq )
-         d# 8 lshift                                    ( seq' )
-      loop                                              ( seq )
-      drop                                              ( )
+      decode-string 2nip                                ( list$ )
+      begin  dup  while                                 ( list$ )
+         ascii , left-parse-string                      ( list$' dev#$ )
+         push-decimal  $number  if  0  then  pop-base   ( list$ port# )
+         (hub-selftest)  if  drop true exit  then       ( list$ )
+      repeat                                            ( list$ )
+      2drop
    then                                                 ( )
 
    \ Maybe need to reset the entire hub here

cpu/arm/olpc/1.75/usb.fth
@@ -21,7 +21,7 @@ purpose: USB elaborations for the BIOS loaded OFW
    \ Port 2 is right upper
    \ Port 3 is left
    \ Port 4 is right lower
-   h# 03.04.02.00  " hub-port-seq" integer-property
+   " 3,4,2" " usb-hub-test-list" string-property
 
 end-package
 

commit 06ea20b704ab23b99d154b9825b99384fce8e29a

OLPC - stack imbalance after (hub-selftest) fails causes test-all failure, regression introduced in r2412 by me
git-svn-id: svn://coreboot.org/openfirmware@3391 1552c027-8020-0410-b4b5-a757f869b4ce

dev/usb2/hcd/probehub.fth
@@ -230,7 +230,7 @@ external
          base @ >r decimal                              ( list$' dev#$ )
          $number  if  0  then                           ( list$' port# )
          r> base !                                      ( list$ port# )
-         (hub-selftest)  if  drop true exit  then       ( list$ )
+         (hub-selftest)  if  2drop true exit  then      ( list$ )
       repeat                                            ( list$ )
       2drop                                             ( )
    then                                                 ( )