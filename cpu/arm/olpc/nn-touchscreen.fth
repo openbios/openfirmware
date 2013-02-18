openbios/openfirmware

commit 6710af181a14de3ace3fcc05e47817868f8449e4

OLPC XO-4 - touchscreen, avoid mouse test targets when using test phase 4, reported by Chia-Hsiu.
git-svn-id: svn://coreboot.org/openfirmware@3535 1552c027-8020-0410-b4b5-a757f869b4ce

cpu/arm/olpc/nn-touchscreen.fth
@@ -905,7 +905,7 @@ d# 30000 value test-timeout
    cursor-off
    consume
    flush
-   background
+   blacken
    -1 to remaining
 ;
 

cpu/arm/olpc/touchscreen-common.fth
@@ -111,8 +111,12 @@ false value right-hit?
 
 : undot  ( -- )  pixcolor @  dup dimmer  " replace-color" $call-screen  ;
 
-: background  ( -- )
+: blacken  ( -- )
    black  0 0  screen-w screen-h  fill-rectangle-noff
+;
+
+: background  ( -- )
+   blacken
    targets?  if
       false to left-hit?
       false to right-hit?

commit 024a97d6e56bdf9d24f3b088e31b3e72c44a237f

OLPC XO-4 - touchscreen, restore missing test instructions during scribble and axes tests, fixes a regression introduced in r3535.  #12553.
git-svn-id: svn://coreboot.org/openfirmware@3556 1552c027-8020-0410-b4b5-a757f869b4ce

cpu/arm/olpc/nn-touchscreen.fth
@@ -906,6 +906,7 @@ d# 30000 value test-timeout
    consume
    flush
    blacken
+   .tsmsg
    -1 to remaining
 ;
 