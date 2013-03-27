openbios/openfirmware

commit 8109f2b5f4351cc57088dff4e96dbdc1a80a17f3

OLPC - set the month and day registers of the RTC when it is being initialised on XO-1.75 and XO-4, and add initialisation in response to RTC power loss on XO-1 and XO-1.5.
git-svn-id: svn://coreboot.org/openfirmware@3608 1552c027-8020-0410-b4b5-a757f869b4ce

cpu/arm/olpc/rtc.fth
@@ -14,7 +14,10 @@ headerless
 
 : ?clear
    h# 3f rtc@  h# 3e rtc@  bwjoin  h# 55aa  <>  if
-      h# 20 8 rtc!                     \ century
+      h# 20 h#  8 rtc!                 \ century
+      h# 13 h#  9 rtc!                 \ year
+      h#  1 h#  8 rtc!                 \ month
+      h#  1 h#  7 rtc!                 \ day
       h# 20 h# 10  do  0 i rtc!  loop  \ wipe cmos@ cmos! area
       h# 55aa  wbsplit  h# 3e rtc!  h# 3f rtc!
       ." RTC SRAM cleared" cr

dev/ds1385r.fth
@@ -88,6 +88,13 @@ h# 80 constant battery-error-bit
    cr  
    then
 ;
+: reinit
+   h# 20 h# 1a rtc!
+   h# 13 h#  9 rtc!
+   h#  1 h#  8 rtc!
+   h#  1 h#  7 rtc!
+   ." RTC cleared" cr
+;
 true value first-open?
 headers
 : open  ( -- true )
@@ -96,7 +103,7 @@ headers
       rega-mode d# 10 rtc!
       regb-mode d# 11 rtc!
       \ If the battery is bad, display a message, but go open the device anyway
-      check-battery battery-message drop
+      check-battery battery-message  if  reinit  then
       false to first-open?
    then
    true

commit 0e4e4657cff1f53892b48fda6293b2a8b78c8fc3

OLPC - avoid use of RTC undefined data, by detecting power loss and clearing the RTC count.
XO-1 and XO-1.5 (ds1385): clear all the RTC count registers, not only
the date.

XO-1.75 and XO-4 (ds1338): force the century to 20 like on previous
models, check and clear the oscillator stop flag, clear all the RTC
count registers, separate the clearing of RTC SRAM from the clearing
of RTC count, and fix a regression in r3608 where the wrong register
offsets were used.



git-svn-id: svn://coreboot.org/openfirmware@3631 1552c027-8020-0410-b4b5-a757f869b4ce

cpu/arm/olpc/rtc.fth
@@ -12,12 +12,20 @@ h# 68 1 reg
 
 headerless
 
+: reinit
+   h# 20 h#  8 rtc! \ century
+   h# 13 h#  6 rtc! \ year
+   h#  1 h#  5 rtc! \ month
+   h#  1 h#  4 rtc! \ day of month
+   h#  2 h#  3 rtc! \ day of week, monday
+   h#  0 h#  2 rtc! \ hours
+   h#  0 h#  1 rtc! \ minutes
+   h#  0 h#  0 rtc! \ seconds
+   ." RTC cleared" cr
+;
+
 : ?clear
    h# 3f rtc@  h# 3e rtc@  bwjoin  h# 55aa  <>  if
-      h# 20 h#  8 rtc!                 \ century
-      h# 13 h#  9 rtc!                 \ year
-      h#  1 h#  8 rtc!                 \ month
-      h#  1 h#  7 rtc!                 \ day
       h# 20 h# 10  do  0 i rtc!  loop  \ wipe cmos@ cmos! area
       h# 55aa  wbsplit  h# 3e rtc!  h# 3f rtc!
       ." RTC SRAM cleared" cr
@@ -44,6 +52,12 @@ headers
       drop true                ( true )
    then                        ( okay? )
 
+   \ check and clear the oscillator stop flag
+   7 rtc@ h# 20 and  if
+      reinit
+      7 rtc@ h# 20 invert and 7 rtc!
+   then
+
    \ manage legacy RTC CMOS usage
    ?clear
 
@@ -69,6 +83,7 @@ headers
    bcd-time&date  >r >r >r >r >r >r
    bcd>  r> bcd>  r> bcd>  r> bcd>  r> bcd>  r> bcd>  r> bcd> ( s m h d m y c )
 
+   d# 20 max
    d# 100 * +  		\ Merge century with year
 ;
 : set-time  ( s m h d m y -- )

dev/ds1385r.fth
@@ -89,10 +89,14 @@ h# 80 constant battery-error-bit
    then
 ;
 : reinit
-   h# 20 h# 1a rtc!
-   h# 13 h#  9 rtc!
-   h#  1 h#  8 rtc!
-   h#  1 h#  7 rtc!
+   h# 20 h# 1a rtc! \ century
+   h# 13 h#  9 rtc! \ year
+   h#  1 h#  8 rtc! \ month
+   h#  1 h#  7 rtc! \ day of the month
+   h#  2 h#  6 rtc! \ day of week, monday
+   h#  0 h#  4 rtc! \ hours
+   h#  0 h#  2 rtc! \ minutes
+   h#  0 h#  0 rtc! \ seconds
    ." RTC cleared" cr
 ;
 true value first-open?
@@ -137,13 +141,7 @@ headers
    bcd-time&date  >r >r >r >r >r >r
    bcd>  r> bcd>  r> bcd>  r> bcd>  r> bcd>  r> bcd>  r> bcd> ( s m h d m y c )
 
-   \ We allow the century byte to force the year to 20xx, but not to force
-   \ it to 19xx, because that would cause a problem when the century
-   \ rolls over.
-   dup  d# 20 <>  if
-      drop  dup d# 94 <  if  d# 20  else  d# 19  then
-   then
-
+   d# 20 max
    d# 100 * +  		\ Merge century with year
 ;
 : set-time  ( s m h d m y -- )