openbios/openfirmware

commit e60699ffd3104cabf1e22dd752a00fefa9d8331b

OLPC XO-4 - force operator abandon of SD card test to cause overall test sequence abort
git-svn-id: svn://coreboot.org/openfirmware@3681 1552c027-8020-0410-b4b5-a757f869b4ce

dev/mmc/sdhci/selftest.fth
@@ -103,7 +103,13 @@ external
    set-unit
 
    wait-card?  if
-      external? 0= exit  \ Unpopulated external SD is not an error
+      \ Operator may abandon test when card is not present ...
+      test-station 6 = if
+         external? 0=  \ ... is not an error for unpopulated external SD
+      else
+         true  \ ... is always an error in SMT or RUNIN
+      then
+      exit
    then
 
    open 0=  if  ." Open SD card failed" cr true exit  then

commit 3301b407b32bf352da217d83dda8e3f0938e302b

OLPC XO-1.5 - fix build broken by r3681, test-station is not defined inside FCode drivers
git-svn-id: svn://coreboot.org/openfirmware@3682 1552c027-8020-0410-b4b5-a757f869b4ce

dev/mmc/sdhci/selftest.fth
@@ -98,13 +98,19 @@ headers
    false
 ;
 
+[ifdef] test-station
+: ship?  test-station 6 =  ;
+[else]
+: ship?  true  ;
+[then]
+
 external
 : selftest  ( -- error? )
    set-unit
 
    wait-card?  if
       \ Operator may abandon test when card is not present ...
-      test-station 6 = if
+      ship?  if
          external? 0=  \ ... is not an error for unpopulated external SD
       else
          true  \ ... is always an error in SMT or RUNIN